de8ac52d856a3df8089684fee85a917e6a61b5fd
Haerang Lee <82368502+LHR-kr@users.noreply.github.com>
2024-05-06 17:32:08 +0900

    화살 충돌 처리 안 되던 버그 수정
    rigidbody의 simulated 프로퍼티를 off 해서 생긴 문제였음. simulated on으로 바꿈

--- Assets/Script/ArrowComponent.cs
+++ Assets/Script/ArrowComponent.cs
@@ -9,7 +9,7 @@ public class ArrowComponent : MonoBehaviour
     private GameObject owner;
     private float attackDamage;
     [SerializeField] private float speed = 1000.0f;
-
+    private Rigidbody2D rigidbody; 
     
     public float AttackDamage
     {
@@ -33,14 +33,16 @@ public class ArrowComponent : MonoBehaviour
     private void Start()
     {
         StartCoroutine(DestroyGameObject());
+        rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+  private void FixedUpdate()
     {
-        transform.position += (transform.TransformDirection(Vector3.right)* speed * Time.deltaTime);
+        Vector2 velocityVector = transform.TransformDirection(Vector3.right) * Time.fixedDeltaTime * speed;
+        Debug.Log(rigidbody);
+        rigidbody.MovePosition(rigidbody.position + velocityVector);
     }
-    
+
     void OnTriggerEnter2D(Collider2D other)
     {
         Debug.Log("arrow hit");
@@ -61,5 +63,5 @@ public class ArrowComponent : MonoBehaviour
         yield return new WaitForSeconds(3.0f);
         Destroy(this.gameObject);
     }
-    
 }
+
