9824fb27a8cf84b181b007db39b2362ad971840b
Haerang Lee <82368502+LHR-kr@users.noreply.github.com>
2024-05-10 20:59:13 +0900

    드래그 기능 추가
    캐릭터 위로 드래그 하면 그 캐릭터와 위치 교체

--- Assets/Script/DragableComponent.cs
+++ Assets/Script/DragableComponent.cs
@@ -34,13 +34,14 @@ public class DragableComponent : MonoBehaviour, IGameStartEventListener,IGameRes
         if (!isDragable) return;
         Vector3 rayStartPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         
-        // 마우스 클릭 뗐을 때, 마우스 커서 아래에 자기 자신이 아닌 다른 캐릭터가 있다면 드래그 드랍 안 되도록 한다.
+        // 마우스 클릭 뗐을 때, 마우스 커서 아래에 자기 자신이 아닌 다른 캐릭터가 위치 교체
         RaycastHit2D[] hitCharacter = Physics2D.RaycastAll(rayStartPos, transform.forward, Mathf.Infinity, LayerMask.GetMask("Character"));
         foreach(RaycastHit2D hit in hitCharacter)
         {
             if (hit.collider.gameObject != this.gameObject)
             {
-                transform.position = PrevPos;
+                transform.position = hit.transform.position;
+                hit.transform.position = PrevPos;
                 return;
             }
         }
